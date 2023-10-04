a2ffd0afac76a3383eb30fcfb334c23c3ae2b9f9
jsiqueira <usernamejoaozinho@gmail.com>
2023-10-04 16:39:19 -0300

    1stlistadetarefas

--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const EditTodoForm = () => {
-  return (
-    <div>EditTodoForm</div>
+export const EditTodoForm = ({editTodo, task}) => {
+   const [valor, setValor] = useState(task.task)
+
+   const handleSubmit = e => {
+    e.preventDefault();
+
+    editTodo(valor, task.id);
+
+    setValor("")
+   }
+
+    return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' 
+        className='todo-input'
+        value={valor} 
+        placeholder='Atualizar tarefa' 
+        onChange={(e) => setValor(e.target.value)}/>
+        <button type='submit' className='todo-btn'>
+            Atualizar tarefa
+        </button>
+    </form>
   )
 }
